7e9436939b74c8cea954273b55e27d46bcd089f9
alekseibulatov <bulatov2803@gmail.com>
2024-11-06 22:25:38 +0300

    Date. Отображение даты.

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime currentDate = item.getCreated();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(String.format("Текущая дата: %s", currentDate.format(dateTimeFormatter)));
+    }
+}
