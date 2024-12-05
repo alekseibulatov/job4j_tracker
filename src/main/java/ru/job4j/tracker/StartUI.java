54a49108e9bbdb163785aeb0e5af94bf4464b90d
alekseibulatov <bulatov2803@gmail.com>
2024-12-06 01:45:07 +0300

    toString

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,5 +10,7 @@ public class StartUI {
         LocalDateTime currentDate = item.getCreated();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(String.format("Текущая дата: %s", currentDate.format(dateTimeFormatter)));
+        Item itemVerifyToString = new Item(241, "Ivanov");
+        System.out.println(itemVerifyToString);
     }
 }
