27575b1f1d12be6ebb8793f47b08089f5808b536
alekseibulatov <bulatov2803@gmail.com>
2024-12-08 20:05:06 +0300

    fix setId

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -39,7 +39,7 @@ public class Tracker {
         int index = indexOf(id);
         if (index != -1) {
             items[index] = item;
-            items[index].setId(id);
+            item.setId(id);
             return true;
         }
         return false;
