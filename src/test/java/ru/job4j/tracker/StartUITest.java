4b274e102744503df125b39ea4ebdaed88d98853
alekseibulatov <bulatov2803@gmail.com>
2025-01-18 01:11:12 +0300

    fix Тестирование. Подготовка данных

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -36,8 +36,7 @@ public class StartUITest {
         Item item = new Item("deleted item");
         tracker.add(item);
         String[] answers = {
-                String.valueOf(item.getId()),
-                "deleted item"
+                String.valueOf(item.getId())
         };
         StartUI.deleteItem(new MockInput(answers), tracker);
         Item deletedItem = tracker.findById(item.getId());
